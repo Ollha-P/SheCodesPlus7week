c34fb4c11080cb3476c27530e8b0c3c3d2fd223b
Ollha-P <pokucuk@gmail.com>
2023-04-04 14:52:17 +0300

    Added element description. Styled in CSS

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "0f08f928cbdc9a8e6475f1d00f033762";
