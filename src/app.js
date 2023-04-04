9b804a22fbb03bc53b56c7e4c0b58a74162a330b
Ollha-P <pokucuk@gmail.com>
2023-04-04 14:42:46 +0300

    Worked on function temperature and city. Displayed it on the app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 function displayTemperature(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
 }
 
 let apiKey = "0f08f928cbdc9a8e6475f1d00f033762";
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=0f08f928cbdc9a8e6475f1d00f033762&units=metric";
 
-axios.get("${apiUrl}&appid=${apiKey}").then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
